422b0fe1e44160118becba91da578b679650eda3
jpmoran-2053942 <2053942@student.swin.edu.au>
2015-09-13 20:25:19 +1000

    Reconverted with files from BlackBoard. Most of the files is /src/ are from my initial conversion, but the files in /src/model are from BlackBoard.

--- src/Model/AIOption.cs
+++ src/Model/AIOption.cs
@@ -1,25 +1,34 @@
+
 using Microsoft.VisualBasic;
 using System;
-namespace BattleShipConversion
+using System.Collections;
+using System.Collections.Generic;
+// using System.Data;
+using System.Diagnostics;
+/// <summary>
+/// The different AI levels.
+/// </summary>
+public enum AIOption
 {
 	/// <summary>
-	/// The different AI levels.
+	/// Easy, total random shooting
 	/// </summary>
-	public enum AIOption
-	{
-		/// <summary>
-		/// Easy, total random shooting
-		/// </summary>
-		Easy,
+	Easy,
 
-		/// <summary>
-		/// Medium, marks squares around hits
-		/// </summary>
-		Medium,
+	/// <summary>
+	/// Medium, marks squares around hits
+	/// </summary>
+	Medium,
 
-		/// <summary>
-		/// As medium, but removes shots once it misses
-		/// </summary>
-		Hard
-	}
+	/// <summary>
+	/// As medium, but removes shots once it misses
+	/// </summary>
+	Hard
 }
+
+//=======================================================
+//Service provided by Telerik (www.telerik.com)
+//Conversion powered by NRefactory.
+//Twitter: @telerik
+//Facebook: facebook.com/telerik
+//=======================================================
